ed57fea3fd2b70e265af6681cc4d766020b6f1f7
Igor Siluianov <olvin.hh@gmail.com>
2020-07-10 03:36:17 +0300

    add menu button; rewrite MainScreen as function component

--- frontend/src/components/MainScreen/MainScreen.js
+++ frontend/src/components/MainScreen/MainScreen.js
@@ -1,49 +1,66 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from "prop-types";
+import classNames from 'classnames';
 
 import Activities from "~/components/MainScreen/Activities/ActivitiesContainer";
 import ActivityLog from "~/components/MainScreen/ActivityLog/ActivityLog";
 import './MainScreen.scss';
 
-export default class MainScreen extends Component {
-    static propTypes = {
-        activityLog: PropTypes.object.isRequired,
-        activities: PropTypes.object.isRequired,
+export default function MainScreen(props) {
+    const { loadActivities, activities, activityLog, logout } = props;
 
-        loadActivities: PropTypes.func.isRequired,
-        logout: PropTypes.func.isRequired,
-    };
-
-    componentDidMount() {
-        const { loadActivities } = this.props;
-
+    useEffect(() => {
         loadActivities();
-    }
+    }, [loadActivities]);
 
-    render() {
-        const { activities, activityLog, logout } = this.props;
+    const [isMenuOpen, setMenuState] = useState(false);
 
-        return (
-            <>
-                <div className="screen-header">
-                    <div className="screen-header__item">
-                        <button className="icon-menu screen-header__menu-button" aria-label="Menu"/>
-                    </div>
-                    <div className="screen-header__item">
-                        <button onClick={logout} className="button-secondary">Log out</button>
-                    </div>
+    return (
+        <div className="screen-wrapper">
+            <div className="screen-header">
+                <div className="screen-header__item">
+                    <button
+                        className={classNames(
+                            'screen-header__menu-button', {
+                                'icon-menu': !isMenuOpen,
+                                'icon-close': isMenuOpen,
+                            }
+                        )}
+                        aria-label="Menu"
+                        onClick={toggleMenuState}
+                    />
                 </div>
-                <div className="screen-body">
-                    <div className="screen-body__block">
-                        <div className="screen-block__title">Today</div>
-                        <Activities />
-                    </div>
-                    <div className="screen-body__block">
-                        <div className="screen-block__title">Recently</div>
-                        <ActivityLog activityLog={activityLog} activities={activities} />
-                    </div>
+                <div className="screen-header__item">
+                    <button onClick={logout} className="button-secondary">Log out</button>
                 </div>
-            </>
-        );
+            </div>
+
+            {isMenuOpen && <div className="screen-menu">
+                menu goes here
+            </div>}
+
+            <div className="screen-body">
+                <div className="screen-body__block">
+                    <div className="screen-body__title">Today</div>
+                    <Activities />
+                </div>
+                <div className="screen-body__block">
+                    <div className="screen-body__title">Recently</div>
+                    <ActivityLog activityLog={activityLog} activities={activities} />
+                </div>
+            </div>
+        </div>
+    );
+
+    function toggleMenuState() {
+        setMenuState(!isMenuOpen);
     }
 }
+
+MainScreen.propTypes = {
+    activityLog: PropTypes.object.isRequired,
+    activities: PropTypes.object.isRequired,
+
+    loadActivities: PropTypes.func.isRequired,
+    logout: PropTypes.func.isRequired,
+};

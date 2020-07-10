3bf99a8fa73bda276ef305626bfecf67a5a6d620
Igor Siluianov <olvin.hh@gmail.com>
2020-07-10 05:00:10 +0300

    fix styles

--- frontend/src/actions/activitiesActions.js
+++ frontend/src/actions/activitiesActions.js
@@ -11,31 +11,31 @@ export const loadActivities = () => (dispatch) => {
     const activitiesMock = {
         100: {
             title: 'Activity 1',
-            colorId: '1',
+            colorId: '8',
         },
         101: {
             title: 'Activity 2',
-            colorId: '2',
+            colorId: '9',
         },
         102: {
             title: 'Activity 3',
-            colorId: '3',
+            colorId: '10',
         },
         103: {
             title: 'Activity 4',
-            colorId: '4',
+            colorId: '11',
         },
         104: {
             title: 'Activity 5',
-            colorId: '5',
+            colorId: '12',
         },
         105: {
             title: 'Activity 6',
-            colorId: '6',
+            colorId: '13',
         },
         106: {
             title: 'Activity 7',
-            colorId: '7',
+            colorId: '14',
         },
     };
 
